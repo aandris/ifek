ee964ddf1ac7828a9fa7cc97fb0839b0f71ae58e
b3domand <b3domand@SZT9-6>
2025-09-16 12:29:31 +0200

    ssssss

--- ifek/Program.cs
+++ ifek/Program.cs
@@ -10,21 +10,7 @@ namespace ifek
     {
         static void Main(string[] args)
         {
-            float szam;
-            Console.WriteLine("Adj meg egy számot: ");
-            szam=Convert.ToInt32(Console.ReadLine());
-
-            if (szam % 2 == 0)
-            {
-                Console.WriteLine("Ez a szám páros");
-            }
-
-            else
-            {
-
-                Console.WriteLine("Ez a szám páratlan");
-
-            }
+            
 
         }
     }
